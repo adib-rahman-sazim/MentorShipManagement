55691e66966f9655b3dff6dce4b86b6d44ab4773
Adib Rahman <adib.rahman@sazim.io>
2026-09-25 21:00:46 +0600

    feat(MMS-31): add the data layer for the permission management screen

--- web/shared/constants/routes.constants.ts
+++ web/shared/constants/routes.constants.ts
@@ -4,6 +4,7 @@ export const SIGN_IN_ROUTE = "/sign-in";
 
 export const DASHBOARD_ROUTE = "/dashboard";
 export const USERS_ROUTE = "/users";
+export const USER_PERMISSIONS_ROUTE = "/users/[userId]/permissions";
 export const SETTINGS_ROUTE = "/settings";
 
 export const PUBLIC_ROUTES = [SIGN_IN_ROUTE] as const;
--- web/shared/redux/rtk-apis/permissions/permissions.api.ts
+++ web/shared/redux/rtk-apis/permissions/permissions.api.ts
@@ -1,8 +1,9 @@
-import { TApiResponse } from "@/shared/typedefs";
+import { IUserPermissionOverridesResponse, TApiResponse } from "@/shared/typedefs";
 
 import projectApi from "../api.config";
 import { CASL_CACHE_TTL_SECONDS } from "./permissions.constants";
 import { IMyCaslRulesResponse } from "./permissions.interfaces";
+import { TReplaceUserPermissionOverridesArgs } from "./permissions.types";
 
 const permissionsApi = projectApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -13,8 +14,41 @@ const permissionsApi = projectApi.injectEndpoints({
       providesTags: ["Permissions"],
       keepUnusedDataFor: CASL_CACHE_TTL_SECONDS,
     }),
+
+    getUserPermissionOverrides: builder.query<IUserPermissionOverridesResponse, string>({
+      query: (userId) => `permissions/users/${userId}/overrides`,
+      transformResponse: (response: TApiResponse<IUserPermissionOverridesResponse>) =>
+        response.data,
+      providesTags: (_result, _error, userId) => [{ type: "UserPermissions" as const, id: userId }],
+    }),
+
+    replaceUserPermissionOverrides: builder.mutation<
+      IUserPermissionOverridesResponse,
+      TReplaceUserPermissionOverridesArgs
+    >({
+      query: ({ userId, ...body }) => ({
+        url: `permissions/users/${userId}/overrides`,
+        method: "PUT",
+        body,
+      }),
+      transformResponse: (response: TApiResponse<IUserPermissionOverridesResponse>) =>
+        response.data,
+      async onQueryStarted({ userId }, { dispatch, queryFulfilled }) {
+        const saved = await queryFulfilled.catch(() => null);
+
+        if (saved) {
+          dispatch(
+            permissionsApi.util.upsertQueryData("getUserPermissionOverrides", userId, saved.data),
+          );
+        }
+      },
+    }),
   }),
   overrideExisting: false,
 });
 
-export const { useGetMyPermissionsQuery } = permissionsApi;
+export const {
+  useGetMyPermissionsQuery,
+  useGetUserPermissionOverridesQuery,
+  useReplaceUserPermissionOverridesMutation,
+} = permissionsApi;
